c87e59e8e08f70686c50aeed90d37c30275462e8
yourboyfriendsdrug <74487159+yourboyfriendsdrug@users.noreply.github.com>
2021-12-12 17:48:25 +0300

    Сделали гет

---
+++ Rubicon-Server-Two/Rubicone-Server-Two.DataAccess.Core/Interfaces/DbContext/IRubicContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rubicone_Server_Two.DataAccess.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rubicone_Server_Two.DataAccess.Core.Interfaces.DbContext
+{
+    public interface IRubicContext : IDisposable, IAsyncDisposable
+    {
+        DbSet<UserRto> Users { get; set; }
+        DbSet<UserRoleRto> UserRoles { get; set; }
+        Task<int> SaveChangeAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Rubicon-Server-Two/Rubicone-Server-Two.DataAccess/DbContext/RubicContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rubicone_Server_Two.DataAccess.Core.Interfaces.DbContext;
+using Rubicone_Server_Two.DataAccess.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rubicone_Server_Two.DataAccess.DbContext
+{
+    public class RubicContext : Microsoft.EntityFrameworkCore.DbContext, IRubicContext
+    {
+        public RubicContext(DbContextOptions<RubicContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserRto> Users { get; set; }
+        public DbSet<UserRoleRto> UserRoles { get; set; }
+
+        public Task<int> SaveChangeAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
